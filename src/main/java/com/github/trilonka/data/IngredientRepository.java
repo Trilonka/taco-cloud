fa56a05504ca234c3be63a5ee996a63a8badef00
trilonka <familiarik@yandex.ru>
2023-03-12 12:05:34 +0300

    Chapter 3: spring-data-jdbc -> spring-data-jpa

--- src/main/java/com/github/trilonka/data/IngredientRepository.java
+++ src/main/java/com/github/trilonka/data/IngredientRepository.java
@@ -1,9 +1,9 @@
 package com.github.trilonka.data;
 
 import com.github.trilonka.dto.Ingredient;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+public interface IngredientRepository extends JpaRepository<Ingredient, String> {
 }
--- src/main/java/com/github/trilonka/data/OrderRepository.java
+++ src/main/java/com/github/trilonka/data/OrderRepository.java
@@ -1,9 +1,9 @@
 package com.github.trilonka.data;
 
 import com.github.trilonka.dto.TacoOrder;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+public interface OrderRepository extends JpaRepository<TacoOrder, Long> {
 }
